9ece02cb24122e532210ecf5df7d160bcf5dd07f
Philip Carpenter <philipcraig.carpenter@gmail.com>
2023-09-14 02:29:37 +0200

    editing midiclock function

--- main.py
+++ main.py
@@ -42,7 +42,7 @@ def sendMidiClock(song: Song):
     try:
         # Calculate the time interval (in seconds) between MIDI clock messages based on the tempo
         interval = 60.0 / (song.tempo * 24)  # 24 MIDI clock messages per quarter note
-        with outPort:
+        with outPort != None:
             print(f"Sending MIDI clock messages on {outPort.name} for Song:",f"{song.songname}",f"Tempo: {song.tempo}","...")
             # Send MIDI clock messages periodically to simulate the tempo
             totalTime = 0
@@ -55,7 +55,7 @@ def sendMidiClock(song: Song):
                     print(f"Stopped sending MIDI clock messages on {outPort.name}...")
                     break
     except Exception as e:
-        print(f"\n{e}")
+        print(f"\n{e.with_traceback}")
 
 if __name__ == "__main__":
 
