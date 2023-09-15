a8f870e8871ead8857c1644d5574be01b44f761f
Philip Carpenter <philipcraig.carpenter@gmail.com>
2023-09-15 08:56:38 +0200

    getting rid of some prints

--- main.py
+++ main.py
@@ -20,7 +20,6 @@ class ClockTimer:
 
     @classmethod
     def send_clock_pulse(cls, port: BaseOutput):
-        #print("F8")
         port.send(mido.Message('clock'))
 
     @property
@@ -53,12 +52,12 @@ class ClockTimer:
         self.timer.cancel()
 
 def sendMidiClock3(song: Song):
-    print(f"PROCESSING: Sending MIDI clock messages on {outPort.name} for Song:",f"{song.songname}",f"Tempo: {song.tempo}","...")
+    #print(f"PROCESSING: Sending MIDI clock messages on {outPort.name} for Song:",f"{song.songname}",f"Tempo: {song.tempo}","...")
     clock = ClockTimer(outPort, tempo=song.tempo)
     clock.start()
     time.sleep(10)    
     clock.stop()
-    print(f"DONE: Stopped sending MIDI clock messages on {outPort.name}...")
+    #print(f"DONE: Stopped sending MIDI clock messages on {outPort.name}...")
     
 
 def sendMidiClock(song: Song):
