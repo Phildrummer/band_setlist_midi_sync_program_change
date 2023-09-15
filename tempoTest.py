5a5b0d62726b1603830a62e244882742e63e43e1
Philip Carpenter <philipcraig.carpenter@gmail.com>
2023-09-15 12:51:00 +0200

    adjusted tempo for one song. added shift

--- tempoTest.py
+++ tempoTest.py
@@ -5,5 +5,5 @@ if __name__ == "__main__":
 # get the ports
     spdSxInPort, spdSxOutPort = CT.getMidiInOutPorts("SPD-SX")
     for bpm in range(50, 201, 10):
-        CT.sendMidiClock(spdSxOutPort, bpm)
+        CT.sendMidiClock(spdSxOutPort,0.024 + bpm + 0.000102*bpm^2)
     
