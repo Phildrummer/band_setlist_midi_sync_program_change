cecc08debeccd838cca9331064263be4ff7c28e1
Philip Carpenter <philipcraig.carpenter@gmail.com>
2023-09-15 12:24:34 +0200

    no shift in tempotest

--- tempoTest.py
+++ tempoTest.py
@@ -5,5 +5,5 @@ if __name__ == "__main__":
 # get the ports
     spdSxInPort, spdSxOutPort = CT.getMidiInOutPorts("SPD-SX")
     for bpm in range(50, 201, 10):
-        CT.sendMidiClock(spdSxOutPort, bpm + 3)
+        CT.sendMidiClock(spdSxOutPort, bpm)
     
