8b7953af250650724247c36594e14a8534552198
Philip Carpenter <philipcraig.carpenter@gmail.com>
2023-09-23 13:59:17 +0200

    different lcd init method

--- main.py
+++ main.py
@@ -39,11 +39,12 @@ def sendMidiClock(song: Song):
 
 if __name__ == "__main__":
 
-    lcd = CharLCD(i2c_expander='PCF8574', address=0x27, port=1,
-              cols=20, rows=4, dotsize=8,
-              charmap='A02',
-              auto_linebreaks=True,
-              backlight_enabled=True)
+    lcd = CharLCD('PCF8574', 0x27)
+    # lcd = CharLCD(i2c_expander='PCF8574', address=0x27, port=1,
+    #           cols=20, rows=4, dotsize=8,
+    #           charmap='A02',
+    #           auto_linebreaks=True,
+    #           backlight_enabled=True)
     lcd.write_string('Hello world')
     # Get the current working directory
     current_directory = os.getcwd() 
